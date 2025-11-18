04a1d5d31d648b746227ad54de4e588f678c7119
Michael Estiven Reyes Escobar <maik1234reyes@gmail.com>
2025-11-18 15:10:14 -0500

    Cambio de maquinas
    Creación de botones para cambiar las maquinas que van a los minijuegos

---
+++ Assets/Scripts/UI/ChangeMachineButton.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ChangeMachineButton : MonoBehaviour
+{
+
+    int index = 0;
+    int prevIndex;
+    public GameObject[] machines;
+
+    void Start()
+    {
+        machines[0].SetActive(true);
+    }
+
+    public void UpMachine()
+    {
+        prevIndex = index;
+
+        index++;
+        if (index == machines.Count())
+        {
+            index = 0;
+        }
+
+        Active();
+
+        Debug.LogWarning(prevIndex);
+    }
+
+    public void DownMachine()
+    {
+        prevIndex = index;
+
+        index--;
+        if (index < 0)
+        {
+            index = machines.Count() - 1;
+        }
+
+        Active();
+
+        Debug.LogWarning(prevIndex);
+    }
+
+    void Active()
+    {
+        machines[index].SetActive(true);
+        machines[prevIndex].SetActive(false);
+    } 
+
+}
